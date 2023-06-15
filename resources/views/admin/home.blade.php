6563c33dac957b8f47fb20f3903bbb6383e81305
Devfrank94 <francescomurro94@gmail.com>
2023-06-15 20:06:34 +0200

    create dashboard controller and new admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    Home Admin Dashboard
                 </div>
             </div>
         </div>
